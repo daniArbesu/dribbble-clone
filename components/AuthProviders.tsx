20c0d3e32475d6a4c7160f6be17af435f1d5db4d
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-31 17:50:15 +0200

    feat: add basic authprovider structure

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,5 +1,35 @@
+"use client"
+import { getProviders, signIn } from "next-auth/react";
+import {useState, useEffect} from "react"
+
+interface Provider {
+  id: string
+  name: string
+  type: string
+  signinUrl: string
+  callbackUrl: string
+  signinUrlParams?: Record<string, string> | null
+}
+
+type Providers = Record<string, Provider>
+
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null)
+
+useEffect(()=>{
+
+  const fetchProviders =async () => {
+    const res = await getProviders()
+    setProviders(res)
+  }
+  fetchProviders()
+},[])
+
+  if(providers != null){
+    return (<div>
+      {Object.values(providers).map((provider:Provider, index) => (<button key={index}>{provider.id}</button>))}
+    </div>)
+  }
 };
 
 export default AuthProviders;
