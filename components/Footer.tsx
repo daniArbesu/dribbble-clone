1e0eb4c593235b1215e687f259998e447265fa0a
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-31 17:01:56 +0200

    feat: add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,24 @@
+import Image from 'next/image';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src={'/logo-purple.svg'}
+            width={115}
+            height={38}
+            alt="footer logo"
+          />
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            Flexibble is the world&apos;s leading community for creatives to
+            share, grow, and get hired.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
