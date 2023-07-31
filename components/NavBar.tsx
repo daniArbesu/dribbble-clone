c44aac5de8e897b03b334d8a04ad7140aeec5a9f
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-31 16:48:41 +0200

    feat: add basic navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,36 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { NavLinks } from '@/constants';
+import AuthProviders from './AuthProviders';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const session = {};
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="logo.svg" alt="Flexibble logo" width={115} height={43} />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map(({ href, text }, index) => (
+            <Link href={href} key={index}>
+              {text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
